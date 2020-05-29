43a5317fed8c90e75f333776efda7716489c4fa2
Andrey Ivanov <aivanov@cinimex.ru>
2020-05-29 11:41:15 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const ntpServer = "0.beevik-ntp.pool.ntp.org"
+
 func main() {
-	// Place your code here
+	timeCurrent := time.Now()
+	timeExact, err := ntp.Time(ntpServer)
+	if err != nil {
+		log.Fatalf("Ошибка запроса: %e", err)
+	}
+	fmt.Printf("current time: %s\n", timeCurrent.Round(time.Second).String())
+	fmt.Printf("exact time: %s\n", timeExact.Round(time.Second).String())
 }
